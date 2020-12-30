3b574dd6bba4100552a273f1b8b9d0d551920785
Steelnox <eric14pastor@gmail.com>
2020-12-30 12:22:39 +0100

    Milestone 1 realizada

---
+++ UD6_M3/UD6_M3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UD6_M3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Milestone1 m1 = new Milestone1();
+
+            string opcion = "";
+
+            Console.WriteLine("Escribe una opcion entre la 1 y la 3");
+            opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    m1.Principal();
+                    break;
+                default:
+                    Console.WriteLine("No es una opcion correcta. Vuelve a intentarlo más tarde");
+                    break;
+            }
+        }
+    }
+}
